cf0673c33b52305e13307cce7c30ad1fc61cdda4
BowenYuan1 <yuanbowen2004@hotmail.com>
2023-07-14 15:06:59 -0700

    Added basic algorithm
    Created a basic algorithm that takes user input and assign players into 2 person groups.

--- Core/Pages/Privacy.cshtml.cs
+++ Core/Pages/Privacy.cshtml.cs
@@ -11,25 +11,57 @@ namespace Core.Pages
 
         public string Description { get; set; }
 
+        public string Input { get; set; }
+
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet() 
         {
-            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
-            ViewData["TimeStamp"] = dateTime;
-
-            this.Description = "this is a test";
-            if (this.Request != null && this.Request.Query.ContainsKey("btn"))
-            {
-                this.Description = "clicked";
-            }
         }
-        
-        protected void Button1_Click(object sender, EventArgs e)
-        {           
+        public void OnPost()
+        {
+            //string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            //ViewData["TimeStamp"] = dateTime;
+            //this.Description = "this is a test";
+            if (this.Request != null && this.Request.Form.ContainsKey("apply"))
+            {
+                if (this.Request.Form.ContainsKey("NameInput"))
+                {
+                    String NameInputs = this.Request.Form["NameInput"];
+                    //String parse = this.Request.Form["clickval"];
+                    //int clickval = int.Parse(parse);
+                    if (NameInputs == null || NameInputs == "" )
+                    {
+                        this.Description = "Input not accepted";
+                        return;
+                    }
+                    String[] NameArr = NameInputs.Split(", ");
+                    Random random = new Random();
+                    for (int i = NameArr.Length - 1; i > 0; i--) 
+                    {
+                        int j = random.Next(i+1);
+                        string temp = NameArr[i];
+                        NameArr[i] = NameArr[j];
+                        NameArr[j] = temp;
+                    }
+                    String SortedNames = "Ladder Pairing: ";
+                    for (int i = 0; i < NameArr.Length - 1; i++) 
+                    {
+                        SortedNames += NameArr[i] + " + " + NameArr[i+1];
+                        
+                        i++;
+                        if (i < NameArr.Length - 2) 
+                        {
+                            SortedNames += ", ";
+                        }
+                    }
+                    this.Description = SortedNames;
+                }
+            }
+                //This is a comment
         }
     }
 }
